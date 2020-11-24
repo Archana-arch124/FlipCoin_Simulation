f56819e66f53e9250c089aef15ab26514178a538
unknown <#archgowda124@gmail.com>
2020-11-24 21:36:30 +0530

    Dispaly Head Or Tail Wins

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -1,2 +1,10 @@
-Welcome to Flip Coin Simulation Problem
+#Welcome to Flip Coin Simulation Problem
+
+toss=$((RANDOM%2))
+if [[ $toss -eq 0 ]]
+   then
+        echo "Head wins"
+   else
+        echo "Tail wins"
+fi
 
