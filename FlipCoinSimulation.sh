98eda35f669feb074ac540cd6f3ea73ba6e092d8
unknown <#archgowda124@gmail.com>
2020-11-24 21:40:20 +0530

    Dispaly multiple times HorT wins

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -1,10 +1,22 @@
 #Welcome to Flip Coin Simulation Problem
 
-toss=$((RANDOM%2))
-if [[ $toss -eq 0 ]]
+num0fHead=0
+numOfTail=0
+declare -A TossCoin
+for ((i=0;i<20;i++))
+do
+   toss=$((RANDOM%2))
+   if [[ $toss -eq 0 ]]
    then
-        echo "Head wins"
+       TossCoin[$i]="T"
+       ((numOfTail++))
    else
-        echo "Tail wins"
-fi
+       TossCoin[$i]="H"
+       ((numOfHead++))
+   fi
+done
+
+echo "Head won $numOfHead times"
+echo "Tail won $numOfTail times"
+echo ${TossCoin[@]}
 
